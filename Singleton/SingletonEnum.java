b2ad33235c9e6aece76eba0ff8f742c759400b00
SankalpManapure <32081017+SankalpManapure@users.noreply.github.com>
2017-09-18 20:16:22 -0400

    Create SingletonEnum.java

---
+++ Singleton/SingletonEnum.java
@@ -0,0 +1,16 @@
+package com.fortinet;
+
+public enum SingletonEnum {
+	
+	INSTANCE;
+	String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
